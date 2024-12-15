c50471e1032148542cac41c80bc197395eb702fa
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2024-12-15 23:41:30 +0530

    feat: setup react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,32 @@ import Navbar from "./components/Navbar";
 import SideBar from "./components/SideBar";
 import Lendings from "./components/Lendings";
 import _404 from "./components/_404";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
   return (
-<Router>
-    <div className="h-screen flex">
-      <SideBar />
-      <main className="flex flex-col flex-grow">
-        <Navbar />
-<Routes>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <div className="h-screen flex">
+          <SideBar />
+          <main className="flex flex-col flex-grow">
+            <Navbar />
+            <Routes>
               <Route path="/" element={<Lendings />} />
               <Route path="/lendings" element={<Lendings />} />
               <Route path="*" element={<_404 />} />
             </Routes>
-      </main>
-    </div>
-</Router>
+          </main>
+        </div>
+      </QueryClientProvider>
+    </Router>
   );
 }
